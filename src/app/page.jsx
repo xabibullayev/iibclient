afade85a9da40989693f6f6330f36a73db7e5b4a
xabibullayev <xabibullayevmm@gmail.com>
2025-02-02 17:14:48 +0500

    filter ready

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,22 @@
 "use client";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import Map, { Marker, Popup } from "react-map-gl";
 import VideocamIcon from "@mui/icons-material/Videocam";
+import RefreshIcon from "@mui/icons-material/Refresh";
 import "mapbox-gl/dist/mapbox-gl.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
+import RecentActorsIcon from "@mui/icons-material/RecentActors";
 
 export default function Home() {
   const [pins, setPins] = useState([]);
   const [type, setType] = useState("");
   const [newPlace, setNewPlace] = useState();
   const [currentPlaceId, setCurrentPlaceId] = useState("");
+
+  const [selectedType, setSelectedType] = useState("all");
+  const [filteredPins, setFilteredPins] = useState(pins);
 
   const handleAddClick = (e) => {
     const { lng, lat } = e.lngLat;
@@ -71,6 +77,14 @@ export default function Home() {
       });
   };
 
+  useEffect(() => {
+    if (selectedType === "all") {
+      setFilteredPins(pins);
+    } else {
+      setFilteredPins(pins.filter((pin) => pin.type === selectedType));
+    }
+  }, [selectedType, pins]);
+
   return (
     <div className={styles.home}>
       <Map
@@ -85,7 +99,7 @@ export default function Home() {
         mapStyle="mapbox://styles/mapbox/streets-v9"
         onDblClick={handleAddClick}
       >
-        {pins.map((pin) => (
+        {filteredPins.map((pin) => (
           <React.Fragment key={pin._id}>
             <Marker longitude={pin.long} latitude={pin.lat} anchor="bottom">
               <div onClick={() => handleMarkerClick(pin._id)}>
@@ -122,15 +136,54 @@ export default function Home() {
                   <option value="" hidden>
                     Camera turi
                   </option>
-                  <option value="hikvision">Hikvision</option>
-                  <option value="skat">Skat</option>
-                  <option value="lachin">Lachin</option>
+                  <option value="ptz">PTZ камера</option>
+                  <option value="obz">Oбзорный камера</option>
+                  <option value="lis">Pаспознавание лиц</option>
+                  <option value="avto">Pаспознавание авто номер</option>
                 </select>
                 <button>Saqlaw</button>
               </form>
             </div>
           </Popup>
         )}
+        <div className={styles.top}>
+          <div className={styles.left}>
+            <div className={styles.all}>
+              <VideocamIcon />
+              <p>{filteredPins.length}</p>
+            </div>
+
+            <div className={styles.type}>
+              <select
+                name=""
+                id=""
+                value={selectedType}
+                onChange={(e) => setSelectedType(e.target.value)}
+              >
+                <option value="all">Hammesi</option>
+                <option value="ptz">PTZ камера</option>
+                <option value="obz">Oбзорный камера</option>
+                <option value="lis">Pаспознавание лиц</option>
+                <option value="avto">Pаспознавание авто номер</option>
+              </select>
+            </div>
+          </div>
+
+          <div className={styles.right}>
+            <div className={styles.users}>
+              <Link href="/users">
+                <RecentActorsIcon />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <button
+          onClick={() => window.location.reload()}
+          className={styles.refreshPage}
+        >
+          <RefreshIcon className={styles.refreshIcon} />
+        </button>
       </Map>
     </div>
   );
---
+++ src/app/users/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.scss";
+
+export default function Users() {
+  return (
+    <div className={styles.users}>
+      <h1>fsfasfa</h1>
+    </div>
+  );
+}
